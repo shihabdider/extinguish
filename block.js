20d2c233287246b435180aa6c34c8e4f052f521e
Shihab Dider <shihabdider@gmail.com>
2024-03-28 07:08:09 -0400

    Added new quotes to the block and updated the fetch URL for quotes.json.

--- block.js
+++ block.js
@@ -1,5 +1,25 @@
+const quotes = [
+  {
+    "quote": "Though one may conquer a thousand times a thousand men in battle, the noblest victor is the one who conquers himself.",
+    "source": "--Dhammapada, verse 103"
+  },
+  {
+    "quote": "He trains himself, having undertaken the training rules, seeing danger in the slightest fault. This is called the training in heightened virtue.",
+    "source": "--AN 3.88"
+  },
+  {
+    "quote": "All is burning. What the all that is burning? The eye is burning. Forms are burning. Consciousness at the eye is burning. Contact at the eye is burning. And whatever there is that arises in dependence on contact at the eye — experienced as pleasure, pain or neither-pleasure-nor-pain — that too is burning.",
+    "source": "--SN 35.28"
+  }
+]
+
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('quotes.json')
+    var candle = document.getElementById('candle');
+    candle.addEventListener('animationend', function() {
+        document.getElementById('quote').style.opacity = 1;
+    });
+
+    fetch('./quotes.json')
         .then(response => response.json())
         .then(quotes => {
             const randomIndex = Math.floor(Math.random() * quotes.length);
